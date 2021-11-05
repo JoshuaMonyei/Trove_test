22656ad80c73112dff96b5ce7edc95d1927cc9ea
Josh <monyeijosh01@gmail.com>
2021-11-05 15:34:52 +0100

    Update paymentRoutes.js

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -5,8 +5,10 @@ const {
 } = require("../controllers/paymentController");
 const { isLoggedIn } = require("../middlewares/userAuth");
 
+//  generate payment link
 router.post("/payments", isLoggedIn, generateUrl);
 
+// generate payment reference
 router.get("/payments/reference/:txRef", getPaymentReference);
 
 module.exports = router;
